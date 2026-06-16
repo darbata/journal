b30f9ec62d182e240c769098a934052c8414f21f
darbata <batarina.darren@gmail.com>
2026-06-16 16:38:13 +1000

    define types for classification

--- emotion_classifier/main.py
+++ emotion_classifier/main.py
@@ -7,11 +7,18 @@ classifier = pipeline("text-classification", model="./models/emotion-english-dis
 class JournalEntry(BaseModel):
     content: str
 
-def classify(entry: str):
+class EmotionScore(BaseModel):
+    label: str
+    score: float
+
+class ClassificationResult(BaseModel):
+    emotions: list[EmotionScore]
+
+def classify(entry: str) -> ClassificationResult:
     return classifier(entry)
 
 app = FastAPI()
 
 @app.post("/api/classify")
 async def classify_journal_entry(entry : JournalEntry):
-    return classify(entry)
+    return classify(entry.content)
