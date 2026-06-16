db5eea4e2599787225d103dc25030fb55a0847f2
darbata <batarina.darren@gmail.com>
2026-06-16 16:33:08 +1000

    load hugging face model

--- emotion_classifier/main.py
+++ emotion_classifier/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from transformers import pipeline
+
+classifier = pipeline("text-classification", model="./models/emotion-english-distilroberta-base/", top_k=None)
 
 class JournalEntry(BaseModel):
     content: str
 
+def classify(entry: str):
+    return classifier(entry)
 
 app = FastAPI()
 
-@app.get("/api/classify")
+@app.post("/api/classify")
 async def classify_journal_entry(entry : JournalEntry):
+    return classify(entry)
