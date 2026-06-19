ecff107b60ed57a0039216c5c8f572095a9e7e87
darbata <batarina.darren@gmail.com>
2026-06-19 16:50:04 +1000

    define function service layer - will add defensive logic...

--- journalapi/src/main/java/io/darbata/journal/services/EntryService.java
+++ journalapi/src/main/java/io/darbata/journal/services/EntryService.java
@@ -1,11 +1,13 @@
 package io.darbata.journal.services;
 
 import io.darbata.journal.dto.EntryDTO;
+import io.darbata.journal.exceptions.EntryNotFoundException;
 import io.darbata.journal.models.Entry;
 import io.darbata.journal.models.UserID;
 import io.darbata.journal.repositories.EntryRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,20 +20,48 @@ public class EntryService {
         this.entryRepository = entryRepository;
     }
 
+    public EntryDTO create(String authorId, String title, String content) {
+        Entry entry = Entry.create(UserID.of(authorId), title, content);
+        this.entryRepository.create(entry);
+        return entryModelToDTO(entry);
+    }
+
+    public EntryDTO findById(UUID id) {
+        Entry entry = this.entryRepository.findById(id)
+                .orElseThrow(() -> new EntryNotFoundException("Entry of id " + id + " was not found"));
+
+        return entryModelToDTO(entry);
+    }
+
     public List<EntryDTO> findAllByUserId(String userId) {
-
-        System.out.println("In service");
-
+        // TODO: time based pagination
+        int paginationLimit = 50;
         UserID user = UserID.of(userId);
-
-        List<Entry> entries = entryRepository.findAllByUserID(user);
-
+        List<Entry> entries = entryRepository.findAllByUserID(user, Instant.now(), paginationLimit);
         return entries.stream()
-                .map((e) -> new EntryDTO(e.getId(), e.getAuthorId().getId(), e.getTitle(), e.getContent(),
-                                e.getCreatedAt(), e.getUpdatedAt())
-                )
+                .map(this::entryModelToDTO)
                 .toList();
+    }
 
+    public EntryDTO updateById(UUID id, String updatedTitle, String updatedContent) {
+        Entry entry = this.entryRepository.findById(id)
+                .orElseThrow(() -> new EntryNotFoundException("Entry of id " + id + " was not found"));
+
+        entry.setTitle(updatedTitle);
+        entry.setContent(updatedContent);
+
+        this.entryRepository.update(entry);
+
+        return entryModelToDTO(entry);
+    }
+
+    public void delete(UUID id) {
+        this.entryRepository.delete(id);
+    }
+
+    private EntryDTO entryModelToDTO (Entry e) {
+        return new EntryDTO(e.getId(), e.getAuthorId().getId(), e.getTitle(), e.getContent(), e.getCreatedAt(),
+                e.getUpdatedAt());
     }
 
 }
