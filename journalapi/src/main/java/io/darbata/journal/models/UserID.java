e6280a97a8b6ee0f2a3b39a161d571d0522bc812
darbata <batarina.darren@gmail.com>
2026-06-19 15:52:15 +1000

    add static factory

--- journalapi/src/main/java/io/darbata/journal/models/UserID.java
+++ journalapi/src/main/java/io/darbata/journal/models/UserID.java
@@ -1,10 +1,20 @@
 package io.darbata.journal.models;
 
 public class UserID {
+
     private String id;
+
+    private UserID (String id) {
+        this.id = id;
+    }
+
+    public static UserID of (String id) {
+        return new UserID(id);
+    }
 
     public String getId() {
         return this.id;
     }
+
 
 }
