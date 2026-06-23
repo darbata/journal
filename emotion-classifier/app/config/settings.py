f058b2b7a279d0f44fb1b266b20fd5ea323dc176
darbata <batarina.darren@gmail.com>
2026-06-23 11:04:09 +1000

    add journalapi to compose, configure networking and env variabvles

---
+++ emotion-classifier/app/config/settings.py
@@ -0,0 +1,11 @@
+import os
+
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    model_path: str # /home/dev/Developer/journal/emotion-classifier/model
+    broker_url: str # amqp://guest:guest@localhost/
+    journal_api_url
+
+
+settings = Settings()
