d3296fc8391efd9de54cc58748f297e500ea5595
darbata <batarina.darren@gmail.com>
2026-06-23 11:07:23 +1000

    pull ml model up out of classification service to avoid circle dependency

--- emotion-classifier/app/services/classification.py
+++ emotion-classifier/app/services/classification.py
@@ -1,28 +1,21 @@
-from fastapi import Depends
+import asyncio
 
-from app.config import settings
-from app.main import ml_models
+from app.config.ml_models import ml_models
+from app.config.rabbit_connection import rabbit_connection
 from app.schemas.classification_event import ClassificationEvent
+from app.schemas.emotion_classification_result import Emotion
 from app.schemas.entry import Entry
 from app.schemas.entry_created_event import EntryCreatedEvent
-from app.services.amqp_publisher import AmqpPublisher
-from app.services.http_client import JournalApiClient
-
-MODEL_PATH = settings.model_path
+from app.services.http_client import journal_api_client
 
 class ClassificationService:
 
-    def classify(self, content: str) -> dict[str, float]:
-        result: list[dict[str, float]] = ml_models["emotion_classifier"](content)
-        return result[0]
+    def classify(self, content: str) -> dict[Emotion, float]:
+        result = ml_models["emotion_classifier"](content)  # list[list[dict]]
+        return {Emotion(r["label"]): r["score"] for r in result[0]}
 
-    def handle_created_entry(
-            self,
-            event: EntryCreatedEvent,
-            client: JournalApiClient = Depends(),
-            publisher: AmqpPublisher = Depends()
-    ) -> None:
-        entry: Entry = client.getEntryById(event.entryId)
-        scores: list[dict[str, float]] = ml_models["emotion_classifier"](entry.content)
-        event = ClassificationEvent(entryId=entry.id, scores=scores)
-        publisher.publish_event(event, "entries.classifications")
+    async def handle_created_entry(self,event: EntryCreatedEvent,) -> None:
+        entry: Entry = await journal_api_client.get_entry_by_id(event.entryId)
+        scores = await asyncio.to_thread(self.classify, entry.content)
+        classification = ClassificationEvent(entryId=entry.id, scores=scores)
+        await rabbit_connection.publish_event(classification, "entries.classifications")
