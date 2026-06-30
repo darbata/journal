a57a9df587a9841629db0f0f266d18cc31f8bb47
darbata <batarina.darren@gmail.com>
2026-06-30 11:47:23 +1000

    add additional map for softer colours (used in backgrounds)

--- journal-client/src/ColourMap.tsx
+++ journal-client/src/ColourMap.tsx
@@ -8,3 +8,14 @@ export const colourMap = {
     surprise: "bg-surprise",
     none: "bg-none",
 };
+
+export const colourMapFaint = {
+    anger: "bg-anger/30",
+    disgust: "bg-disgust/30",
+    fear: "bg-fear/30",
+    joy: "bg-joy/30",
+    neutral: "bg-neutral/30",
+    sadness: "bg-sadness/30",
+    surprise: "bg-surprise/30",
+    none: "bg-none/30",
+};
