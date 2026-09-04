100e116f406b8cd0a7dfc9f7058d0f6dd898f358
darbata <batarina.darren@gmail.com>
2026-09-04 17:21:03 +1000

    add bean validation on title and content for creating journal entry

--- journalapi/src/main/java/io/darbata/journal/dto/CreateEntryRequest.java
+++ journalapi/src/main/java/io/darbata/journal/dto/CreateEntryRequest.java
@@ -1,6 +1,13 @@
 package io.darbata.journal.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record CreateEntryRequest (
+        @NotBlank
+        @Size(max = 32)
         String title,
+        @NotBlank
+        @Size(max = 2048)
         String content
 ) { }
